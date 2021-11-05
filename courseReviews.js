b5836c89d2a3c68cc95fda572680781e7053d56c
amueller8 <68559641+amueller8@users.noreply.github.com>
2021-11-04 21:44:09 -0400

    add professor page
    added professor page and set up js file for reviews (to then generate review boxes)

---
+++ courseReviews.js
@@ -0,0 +1,15 @@
+let professorReviews = [
+    {
+        professor: "Freire",
+        reviews: [
+            "Make sure you’re keeping up in class; easiest to learn it in class than out. She’s very helpful in office hours and good at responding to emails",
+            "Definitely recommend attending her officer hours to get to know her and learn more about the materials. (I understand the concept of recursion after talking with her)"
+
+        ]
+
+    },
+
+
+
+
+];
